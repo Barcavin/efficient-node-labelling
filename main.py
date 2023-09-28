37953ab7c27f3b805a4856bd07bca7ce509b3e55
barcavin <kevindong1994@gmail.com>
2023-09-28 03:54:53 -0400

    citation2

--- main.py
+++ main.py
@@ -31,25 +31,24 @@ def train(encoder, predictor, data, split_edge, optimizer, batch_size,
     predictor.train()
     device = data.adj_t.device()
     criterion = BCEWithLogitsLoss(reduction='mean')
-    pos_train_edge = split_edge['train']['edge'].to(device)
+    source_edge = split_edge['train']['source_node'].to(device)
+    target_edge = split_edge['train']['target_node'].to(device)
+    adjmask = torch.ones_like(source_edge, dtype=torch.bool)
     
     optimizer.zero_grad()
     total_loss = total_examples = 0
-    if dataset_name.startswith("ogbl"):
-        neg_edge_epoch = torch.randint(0, data.adj_t.size(0), data.edge_index.size(), dtype=torch.long,
-                             device=device)
-    else:
-        neg_edge_epoch = negative_sampling(data.edge_index, num_nodes=data.adj_t.size(0))
     # for perm in (pbar := tqdm(DataLoader(range(pos_train_edge.size(0)), batch_size,
     #                        shuffle=True)) ):
-    for perm in DataLoader(range(pos_train_edge.size(0)), batch_size,
+    for perm in DataLoader(range(source_edge.size(0)), batch_size,
                            shuffle=True):
-        edge = pos_train_edge[perm].t()
         if mask_target:
-            adj_t = data.adj_t
-            undirected_edges = torch.cat((edge, edge.flip(0)), dim=-1)
-            target_adj = SparseTensor.from_edge_index(undirected_edges, sparse_sizes=adj_t.sizes())
-            adj_t = spmdiff_(adj_t, target_adj, keep_val=True)
+            adjmask[perm] = 0
+            tei = torch.stack((source_edge[adjmask], target_edge[adjmask]), dim=0) # TODO: check if both direction is removed
+            adj_t = SparseTensor.from_edge_index(tei,
+                               sparse_sizes=(data.num_nodes, data.num_nodes)).to_device(
+                                   source_edge.device, non_blocking=True)
+            adjmask[perm] = 1
+            adj_t = adj_t.to_symmetric()
         else:
             adj_t = data.adj_t
 
@@ -63,7 +62,10 @@ def train(encoder, predictor, data, split_edge, optimizer, batch_size,
         # elif dataset == "collab" or dataset == "ppa":
         #     neg_edge = torch.randint(0, create_input(data).size()[0], edge.size(), dtype=torch.long,
         #                      device=device)
-        neg_edge = neg_edge_epoch[:,perm]
+        dst_neg = torch.randint(0, data.num_nodes, perm.size(),
+                                dtype=torch.long, device=device)
+        edge = torch.stack((source_edge[perm], target_edge[perm]), dim=0)
+        neg_edge = torch.stack((source_edge[perm], dst_neg), dim=0)
         train_edges = torch.cat((edge, neg_edge), dim=-1)
         train_label = torch.cat((torch.ones(edge.size()[1]), torch.zeros(neg_edge.size()[1])), dim=0).to(device)
         out = predictor(h, adj_t, train_edges).squeeze()
@@ -89,86 +91,42 @@ def test(encoder, predictor, data, split_edge, evaluator,
     encoder.eval()
     predictor.eval()
     device = data.adj_t.device()
-    adj_t = data.adj_t
+    adj_t = data.full_adj_t
     h = encoder(data.x, adj_t)
+    def test_split(split):
+        source = split_edge[split]['source_node'].to(device)
+        target = split_edge[split]['target_node'].to(device)
+        target_neg = split_edge[split]['target_node_neg'].to(device)
 
-    # pos_train_edge = split_edge['train']['edge'].to(device)
-    pos_valid_edge = split_edge['valid']['edge'].to(device)
-    neg_valid_edge = split_edge['valid']['edge_neg'].to(device)
-    pos_test_edge = split_edge['test']['edge'].to(device)
-    neg_test_edge = split_edge['test']['edge_neg'].to(device)
+        pos_preds = []
+        for perm in DataLoader(range(source.size(0)), batch_size):
+            src, dst = source[perm], target[perm]
+            pos_preds += [predictor(h, adj_t, torch.stack((src, dst))).squeeze().cpu()]
+        pos_pred = torch.cat(pos_preds, dim=0)
 
-    pos_valid_preds = []
-    for perm in DataLoader(range(pos_valid_edge.size(0)), batch_size):
-        edge = pos_valid_edge[perm].t()
-        out = predictor(h, adj_t, edge)
-        pos_valid_preds += [out.squeeze().cpu()]
-    pos_valid_pred = torch.cat(pos_valid_preds, dim=0)
+        neg_preds = []
+        source = source.view(-1, 1).repeat(1, 1000).view(-1)
+        target_neg = target_neg.view(-1)
+        for perm in DataLoader(range(source.size(0)), batch_size):
+            src, dst_neg = source[perm], target_neg[perm]
+            neg_preds += [predictor(h, adj_t, torch.stack((src, dst_neg))).squeeze().cpu()]
+        neg_pred = torch.cat(neg_preds, dim=0).view(-1, 1000)
 
-    neg_valid_preds = []
-    for perm in DataLoader(range(neg_valid_edge.size(0)), batch_size):
-        edge = neg_valid_edge[perm].t()
-        neg_valid_preds += [predictor(h, adj_t, edge).squeeze().cpu()]
-    neg_valid_pred = torch.cat(neg_valid_preds, dim=0)
+        return evaluator.eval({
+            'y_pred_pos': pos_pred,
+            'y_pred_neg': neg_pred,
+        })['mrr_list'].mean().item()
 
-    start_time = time.perf_counter()
-    if use_valedges_as_input:
-        adj_t = data.full_adj_t
-        h = encoder(data.x, adj_t)
-    if fast_inference:
-        # caching
-        predictor(h, adj_t, None, cache_mode='build')
-        cache_mode='use'
-    else:
-        cache_mode=None
-    pos_test_preds = []
-    for perm in DataLoader(range(pos_test_edge.size(0)), batch_size):
-        edge = pos_test_edge[perm].t()
-        out = predictor(h, adj_t, edge, cache_mode=cache_mode)
-        pos_test_preds += [out.squeeze().cpu()]
-    pos_test_pred = torch.cat(pos_test_preds, dim=0)
+    train_mrr = 0.0 #test_split('eval_train')
+    valid_mrr = test_split('valid')
+    test_mrr = test_split('test')
 
-    neg_test_preds = []
-    for perm in DataLoader(range(neg_test_edge.size(0)), batch_size):
-        edge = neg_test_edge[perm].t()
-        neg_test_preds += [predictor(h, adj_t, edge, cache_mode=cache_mode).squeeze().cpu()]
-    neg_test_pred = torch.cat(neg_test_preds, dim=0)
-    end_time = time.perf_counter()
-    total_time = end_time - start_time
-    print(f'Inference for one epoch Took {total_time:.4f} seconds')
-    if fast_inference:
-        # delete cache
-        predictor(h, adj_t, None, cache_mode='delete')
-    
-    results = {}
-    for K in [10, 20, 50, 100]:
-        evaluator.K = K
-        valid_hits = evaluator.eval({
-            'y_pred_pos': pos_valid_pred,
-            'y_pred_neg': neg_valid_pred,
-        })[f'hits@{K}']
-        test_hits = evaluator.eval({
-            'y_pred_pos': pos_test_pred,
-            'y_pred_neg': neg_test_pred,
-        })[f'hits@{K}']
-
-        results[f'Hits@{K}'] = (valid_hits, test_hits)
-
-
-    valid_result = torch.cat((torch.ones(pos_valid_pred.size()), torch.zeros(neg_valid_pred.size())), dim=0)
-    valid_pred = torch.cat((pos_valid_pred, neg_valid_pred), dim=0)
-
-    test_result = torch.cat((torch.ones(pos_test_pred.size()), torch.zeros(neg_test_pred.size())), dim=0)
-    test_pred = torch.cat((pos_test_pred, neg_test_pred), dim=0)
-
-    results['AUC'] = (roc_auc_score(valid_result.cpu().numpy(),valid_pred.cpu().numpy()),roc_auc_score(test_result.cpu().numpy(),test_pred.cpu().numpy()))
-
-    return results
+    return {"MRR":(valid_mrr, test_mrr)}
 
 def main():
     parser = argparse.ArgumentParser(description='OGBL-DDI (GNN)')
     # dataset setting
-    parser.add_argument('--dataset', type=str, default='collab')
+    parser.add_argument('--dataset', type=str, default='ogbl-collab')
     parser.add_argument('--val_ratio', type=float, default=0.1)
     parser.add_argument('--test_ratio', type=float, default=0.2)
     parser.add_argument('--dataset_dir', type=str, default='./data')
@@ -206,7 +164,7 @@ def main():
     parser.add_argument('--log_steps', type=int, default=20)
     parser.add_argument('--patience', type=int, default=100, help='number of patience steps for early stopping')
     parser.add_argument('--runs', type=int, default=10)
-    parser.add_argument('--metric', type=str, default='Hits@50', help='main evaluation metric')
+    parser.add_argument('--metric', type=str, default='MRR', help='main evaluation metric')
 
     # misc
     parser.add_argument('--log_dir', type=str, default='./logs')
@@ -240,13 +198,9 @@ def main():
     with open(final_log_path, 'a') as f:
         f.write('\n' + cmd_input)
     
-    evaluator = Evaluator(name='ogbl-ddi')
+    evaluator = Evaluator(name=args.dataset)
     loggers = {
-        'Hits@10': Logger(args.runs, args),
-        'Hits@20': Logger(args.runs, args),
-        'Hits@50': Logger(args.runs, args),
-        'Hits@100': Logger(args.runs, args),
-        'AUC': Logger(args.runs, args),
+        'MRR': Logger(args.runs, args)
     }
 
     val_max = 0.0
